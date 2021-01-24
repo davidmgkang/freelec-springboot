7f8b2c4f97bb015ee2828f5aee0d5815fae36995
mingu-kang <mingu.kang@navercorp.com>
2021-01-24 23:25:39 +0900

    PostsApiController가 update 추가

---
+++ src/main/java/com/mingu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.mingu.book.springboot.web;
+
+
+import com.mingu.book.springboot.service.posts.PostService;
+import com.mingu.book.springboot.web.dto.PostsResponseDto;
+import com.mingu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.mingu.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postService.findById(id);
+    }
+}
